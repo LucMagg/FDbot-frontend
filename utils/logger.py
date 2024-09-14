226103d5bb94615ec1ea83fdb19f47645bf4d983
Spirou07 <lefitjunior@hotmail.com>
2024-09-14 11:30:57 +0200

    Add levels related commands

--- utils/logger.py
+++ utils/logger.py
@@ -1,11 +1,10 @@
 from utils.str_utils import str_now
 
 class Logger:
-  def __init__(self) -> None:
-    pass
-
+  @staticmethod
   def command_log(cmd, interaction):
     print(f'[{str_now()}] [COMMAND ] commande {cmd} entrée par {interaction.user} dans le chan {interaction.channel} du serveur {interaction.guild.name}')
 
+  @staticmethod
   def ok_log(cmd):
     print(f'[{str_now()}] [COMMAND ] commande {cmd} exécutée avec succès')
