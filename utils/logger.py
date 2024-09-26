ee876c6207633103491efe3b1d37f047694fa7b7
Spirou07 <lefitjunior@hotmail.com>
2024-09-26 09:10:31 +0200

    small command print change

--- utils/logger.py
+++ utils/logger.py
@@ -3,8 +3,8 @@ from utils.str_utils import str_now
 class Logger:
   @staticmethod
   def command_log(cmd, interaction):
-    print(f'[{str_now()}] [COMMAND ] commande {cmd} entrée par {interaction.user} dans le chan {interaction.channel} du serveur {interaction.guild.name}')
+    print(f'[{str_now()}] [COMMAND] commande {cmd} entrée par {interaction.user} dans le chan {interaction.channel} du serveur {interaction.guild.name}')
 
   @staticmethod
   def ok_log(cmd):
-    print(f'[{str_now()}] [COMMAND ] commande {cmd} exécutée avec succès')
+    print(f'[{str_now()}] [COMMAND] commande {cmd} exécutée avec succès')
