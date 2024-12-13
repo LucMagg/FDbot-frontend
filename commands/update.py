069ca6a62d59be321a3c14dc7a8bdb1defd83732
LucMagg <luc.maggiotto@gmail.com>
2024-12-13 10:29:07 +0100

    refactor /update
    -> adaptation au nouveau pattern du interaction_handler

--- commands/update.py
+++ commands/update.py
@@ -12,7 +12,6 @@ class Update(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
     self.logger = bot.logger
-    self.interaction_handler = InteractionHandler(self.bot)
     self.command = next((c for c in bot.static_data.commands if c['name'] == 'update'), None)
     self.error_msg = Message(bot).message('error')
     self.return_msg = Message(bot).message('update')
@@ -32,12 +31,13 @@ class Update(commands.Cog):
   async def update_app_command(self, interaction: discord.Interaction, type: str):
     self.logger.command_log('update', interaction)
     self.logger.log_only('debug', f"arg : {type}")
-    await self.interaction_handler.handle_response(interaction=interaction, wait_msg=True, more_response=self.return_msg['description']['warning'])
+    self.interaction_handler = InteractionHandler(self.bot)
+    await self.interaction_handler.send_wait_message(interaction=interaction, more_response=self.return_msg['description']['warning'])
     if not type:
       type = 'all'
     
     response = await self.get_response(type, interaction)
-    await self.interaction_handler.handle_response(interaction=interaction, response=response)
+    await self.interaction_handler.send_embed(interaction=interaction, response=response)
     self.logger.ok_log('update')
 
 
