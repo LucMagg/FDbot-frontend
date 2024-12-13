817af281c4f3e116c996ffa5788153ba1d042cc3
LucMagg <luc.maggiotto@gmail.com>
2024-12-13 10:13:59 +0100

    refactor /petlist
    -> adaptation au nouveau pattern du interaction_handler

--- commands/petlist.py
+++ commands/petlist.py
@@ -14,7 +14,6 @@ class Petlist(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
     self.logger = bot.logger
-    self.interaction_handler = InteractionHandler(self.bot)
     self.command = next((c for c in bot.static_data.commands if c['name'] == 'petlist'), None)
     self.error_msg = Message(bot).message('error')
     self.help_msg = Message(bot).help('petlist')
@@ -30,9 +29,10 @@ class Petlist(commands.Cog):
   async def petlist_app_command(self, interaction: discord.Interaction, héros: str):
     self.logger.command_log('petlist', interaction)
     self.logger.log_only('debug', f"arg : {héros}")
-    await self.interaction_handler.handle_response(interaction=interaction, wait_msg=True)
+    self.interaction_handler = InteractionHandler(self.bot)
+    await self.interaction_handler.send_wait_message(interaction=interaction)
     response = await self.get_response(héros, interaction)
-    await self.interaction_handler.handle_response(interaction=interaction, response=response)
+    await self.interaction_handler.send_embed(interaction=interaction, response=response)
     self.logger.ok_log('petlist')
 
   async def get_response(self, héros, interaction):
