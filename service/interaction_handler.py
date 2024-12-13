e24a9734ce5e4d9a1378237668f4de4da1587783
LucMagg <luc.maggiotto@gmail.com>
2024-12-13 09:49:33 +0100

    refactor interaction_handler
    -> split de l'unique fonction handle_response en plusieurs endpoints afin de rendre le code + lisible

--- service/interaction_handler.py
+++ service/interaction_handler.py
@@ -12,50 +12,104 @@ class InteractionHandler:
     self.bot = bot
     self.logger = bot.logger
     self.message = Message(bot)
-    self.error_message = Message(bot).message('error')
     self.original_message_id = None
     self.was_a_modal = False
     self.last_content = None
 
-  async def handle_response(self, interaction: discord.Interaction, response=None, content='', view=None, modal=None, wait_msg=False, more_response='', generic_error_msg=False, timeout=None):
+  async def send_wait_message(self, interaction: discord.Interaction, more_response=''):
+    wait_message = self.message.message('wait')
+    embed = discord.Embed(
+      title=wait_message.get('title'), 
+      description=wait_message.get('description') + more_response,
+      color=get_discord_color(wait_message.get('color')))
+    self.logger.log_only('debug', 'send waiting message')
+    return await self.handle_response(interaction=interaction, embed=embed)
+  
+  async def send_generic_error_message(self, interaction: discord.Interaction):
+    error_message = self.message.message('error')
+    embed = discord.Embed(
+      title=error_message.get('title'),
+      description=error_message.get('description').get('generic')[0].get('text'),
+      color=get_discord_color(error_message.get('color')))
+    self.logger.log_only('debug', 'send generic error message')
+    return await self.handle_response(interaction=interaction, embed=embed)
+  
+  async def send_timeout_message(self, interaction: discord.Interaction, timeout: int):
+    inactivity_time = convert_seconds(timeout)
+    embed = discord.Embed(
+      title='Session expirée ⏰',
+      description=f'La commande a été annulée après {inactivity_time} :shrug:\nMerci de réitérer la commande :wink:',
+      color=get_discord_color('red'))
+    self.logger.log_only('debug', 'send command timeout')
+    return await self.handle_response(interaction=interaction, embed=embed)
+  
+  async def send_embed(self, interaction: discord.Interaction, response: dict):
+    embed = self.build_embed(response)
+    self.logger.log_only('debug', 'send embed')
+    return await self.handle_response(interaction=interaction, embed=embed)
+  
+  async def send_modal(self, interaction: discord.Interaction, modal: discord.ui.Modal):
+    self.logger.log_only('debug', 'send modal')
+    return await self.handle_response(interaction=interaction, modal=modal)
+  
+  async def send_view(self, interaction: discord.Interaction, view: discord.ui.View, content: str = ''):
+    if content == '':
+      content = self.last_content
+    self.logger.log_only('debug', 'send view')
+    return await self.handle_response(interaction=interaction, view=view, content=content)
+  
+  async def send_view_and_embed(self, interaction: discord.Interaction, response: dict, view: discord.ui.View):
+    embed = self.build_embed(response)
+    self.logger.log_only('debug', 'send view & embed')
+    return await self.handle_response(interaction=interaction, embed=embed, view=view)
+
+  def build_embed(self, response: dict):
+    footer_msg = self.message.message('footer')
+    if len(response.get('description')) + len(footer_msg.get('ok')) > 4096:
+      taille_max = 4096 - len(footer_msg.get('ok')) - len(footer_msg.get('too_long'))
+      response['description'] = response.get('description')[0:taille_max] + footer_msg.get('too_long')
+    
+    embed = discord.Embed(
+      title=response.get('title'),
+      description=response.get('description'),
+      color=get_discord_color(response.get('color')))
+
+    if 'image' in response.keys():
+      embed.set_image(url=response.get('image'))
+    if 'thumbnail' in response.keys():
+      embed.set_thumbnail(url=response.get('thumbnail'))
+    self.logger.log_only('debug', 'embed built')
+    return embed
+  
+  async def handle_response(self, interaction: discord.Interaction, embed: discord.Embed = None, content: str = '', view: discord.ui.View = None, modal: discord.ui.Modal = None):
     self.logger.log_only('debug', f'interactionId: {interaction.id}')
+    if embed is not None:
+      embed.set_footer(text=self.message.message('footer').get('ok'))
+
     for attempt in range(max_attempts):
       try:
         if modal is not None:
-          self.logger.log_only('debug', 'modal')
           if hasattr(interaction, 'message') and interaction.message:
             self.original_message_id = interaction.message.id
           self.was_a_modal = True
           return await interaction.response.send_modal(modal)
-
         if not interaction.response.is_done():
           await interaction.response.defer()
-
-        if view is not None and response is None:
-          self.logger.log_only('debug', 'view only')
-          return await self.handle_view_response(interaction, content, view)
-        
-        if response is not None and view is None:
-          self.logger.log_only('debug', 'embed only')
-          embed = self.build_embed(response, wait_msg, more_response, generic_error_msg, timeout)
-          return await self.handle_embed_response(interaction, embed)
-        
-        self.logger.log_only('debug', 'view & embed')
-        embed = self.build_embed(response, wait_msg, more_response, generic_error_msg, timeout)
-        return await self.handle_view_and_embed_response(interaction, embed, view)
+        if view is not None and embed is None:
+          return await self.handle_view_response(interaction=interaction, content=content, view=view)
+        if view is None and embed is None:
+          return await self.handle_embed_response(interaction=interaction, embed=embed)
+        return await self.handle_view_and_embed_response(interaction=interaction, view=view, embed=embed)
 
       except (HTTPException, InteractionResponded) as e:
         if attempt == max_attempts - 1:
           self.logger.log_only('warning', f'Échec de l\'interaction après {max_attempts} tentatives : {e}')
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.5)
 
       except Exception as e:
         self.logger.log_only('warning', f'Erreur d\'interaction non gérée : {e}')
 
-  async def handle_view_response(self, interaction: discord.Interaction, content, view):
-    if content == '':
-      content = self.last_content
-
+  async def handle_view_response(self, interaction: discord.Interaction, content: str, view: discord.ui.View):
     if self.original_message_id and hasattr(interaction, 'message'):
       try:
         original_message = await interaction.channel.fetch_message(self.original_message_id)
@@ -90,7 +144,7 @@ class InteractionHandler:
         return await interaction.response.send_message(content=content, embed=None, view=view)
     
     
-  async def handle_embed_response(self, interaction: discord.Interaction, embed):
+  async def handle_embed_response(self, interaction: discord.Interaction, embed: discord.Embed):
     if self.original_message_id and hasattr(interaction, 'message'):
       try:
         original_message = await interaction.channel.fetch_message(self.original_message_id)
@@ -120,11 +174,11 @@ class InteractionHandler:
         self.logger.log_only('debug', f'erreur : {e}')
         return await interaction.response.send_message(content='', embed=embed, view=None)
   
-  async def handle_view_and_embed_response(self, interaction: discord.Interaction, embed, view):
+  async def handle_view_and_embed_response(self, interaction: discord.Interaction, embed: discord.Embed, view: discord.ui.View):
     if self.original_message_id and hasattr(interaction, 'message'):
       try:
         original_message = await interaction.channel.fetch_message(self.original_message_id)
-        self.logger.log_only('debug', 'embed&view after modal with an original message')
+        self.logger.log_only('debug', 'embed & view after modal with an original message')
         new_message = await original_message.edit(content='', embed=embed, view=view)
         self.original_message_id = None
         self.was_a_modal = False
@@ -134,7 +188,7 @@ class InteractionHandler:
 
     if self.was_a_modal:
       try:
-        self.logger.log_only('debug', 'embed&view after modal with no original message')
+        self.logger.log_only('debug', 'embed & view after modal with no original message')
         self.was_a_modal = False
         return await interaction.followup.send(content='', embed=embed, view=view)
       except Exception as e:
@@ -149,36 +203,3 @@ class InteractionHandler:
       except Exception as e:
         self.logger.log_only('debug', f'erreur : {e}')
         return await interaction.response.send_message(content='', embed=embed, view=view)
-
-  def build_embed(self, response, wait_msg, more_response, generic_error_msg, timeout):
-    if response is None and not wait_msg and not generic_error_msg and not timeout:
-      return None
-
-    if response is not None:
-      footer_msg = self.message.message('footer')
-
-      if len(response.get('description')) + len(footer_msg.get('ok')) > 4096:
-        taille_max = 4096 - len(footer_msg.get('ok')) - len(footer_msg.get('too_long'))
-        response['description'] = response.get('description')[0:taille_max] + footer_msg.get('too_long')
-      embed = discord.Embed(title=response.get('title'), description=response.get('description'), color=get_discord_color(response.get('color')))
-
-      if 'image' in response.keys():
-        embed.set_image(url=response.get('image'))
-      if 'thumbnail' in response.keys():
-        embed.set_thumbnail(url=response.get('thumbnail'))
-
-      embed.set_footer(text=footer_msg.get('ok'))
-    elif wait_msg:
-      tempo_response = self.message.message('wait')
-      embed = discord.Embed(title = tempo_response.get('title'), description = tempo_response.get('description') + more_response, color=get_discord_color(tempo_response.get('color')))
-    elif generic_error_msg:
-      error_response = self.error_message.get('description').get('generic')[0].get('text')
-      embed = discord.Embed(title = self.error_message.get('title'), description = error_response, color=get_discord_color(self.error_message.get('color')))
-    elif timeout is not None:
-      inactivity_time = convert_seconds(timeout)
-      embed = discord.Embed(title='Session expirée ⏰', description=f'La commande a été annulée après {inactivity_time} :shrug:\nMerci de réitérer la commande :wink:', color=get_discord_color('red'))
-      self.logger.log_only('debug', 'command timeout')
-    else:
-      self.logger.log_only('warning', 'foirage de paramètres :)')
-      return None
-    return embed
