4655060d84b27439a31760008180637cd993aaf1
Spirou07 <lefitjunior@hotmail.com>
2025-06-21 12:39:26 +0200

    List of merc by color

--- service/merc.py
+++ service/merc.py
@@ -1,5 +1,5 @@
 from utils.misc_utils import stars
-
+from collections import defaultdict
 
 class MercService:
   def __init__(self, bot):
@@ -20,32 +20,50 @@ class MercService:
     heroes = await self.bot.back_requests.call('getAllHeroes', False)
     if not heroes:
       return {'title': '', 'description': 'Une erreur s\'est produite lors de l\'envoi de la commande :shrug:\nMerci de réitérer la commande :wink:', 'color': 'red'}
-    description = f'# Liste des mercs de {user_doc.get('user')} : # \n'
+    descriptions = [f'# Liste des mercs de {user_doc.get('user')} : #']
     mercs = sorted(user_doc.get('mercs'), key=lambda x: x['name'])
+    mercH = [h for h in heroes if h.get('name_slug') in [m['name_slug'] for m in mercs]]
+
+    mercHByColor = defaultdict(list)
+    for h in mercH:
+      key = h['color']
+      mercHByColor[key].append(h)
+
+    for color, colorHeroes in mercHByColor.items():
+      descriptions.append(self.print_color_merc_details(color, mercs, colorHeroes))
+
+    return {'title': '', 'description': '\n'.join(descriptions), 'color': 'blue'}
+
+  def print_color_merc_details(self, color, mercs, mercsH):
+    to_return = f'## {color} ##\n'
+    details = []
     for merc in mercs:
-      hero = next((h for h in heroes if h.get('name_slug') == merc.get('name_slug')), None)
-      description += f'- {hero.get('name') if hero else merc.get('name')}'
-      details = self.print_merc_details(merc)
-      if details != '':
-        description += f' ({details})'
-      description += '\n'
-    return {'title': '', 'description': description, 'color': 'blue'}
-  
-  def print_merc_details(self, merc):
-    to_return = ' '
+      hero = next((h for h in mercsH if h.get('name_slug') == merc.get('name_slug')), None)
+      if not hero:
+        continue
+      details.append(self.print_merc_details(merc, hero))
+
+    to_return += '\n'.join(details)
+    return to_return
+
+  def print_merc_details(self, merc, hero):
+    name = f'- {hero.get('name') if hero else merc.get('name')}'
+    details = ''
     if merc.get('ascend'):
-      to_return += f'{merc.get('ascend')} '
+      details += f'{merc.get('ascend')} '
     if merc.get('merge'):
-      to_return += f'M{merc.get('merge')} '
+      details += f'M{merc.get('merge')} '
     if merc.get('pet'):
-      to_return += f'avec son pet '
+      details += f'avec son pet '
     if (merc.get('talent_a2') and merc.get('ascend') != 'A3') or merc.get('talent_a3'):
       if merc.get('pet'):
-        to_return += 'et '
+        details += 'et '
       else:
-        to_return += 'avec '
+        details += 'avec '
       if merc.get('talent_a3'):
-        to_return += 'son talent A3 '
+        details += 'son talent A3 '
       else:
-        to_return += 'son talent A2 '
-    return to_return.strip()
+        details += 'son talent A2 '
+    if not details:
+      return name
+    return f'{name} ({details.strip()})'
