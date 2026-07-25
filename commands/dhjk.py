82c215c5780165ab7d7fd0cded21acf662539b69
LucMagg <luc.maggiotto@gmail.com>
2026-07-25 17:21:08 +0200

    update dhjk
    -> refactor to handle the new bot's architecture with v2 features (translations/sessions/choices)

--- commands/dhjk.py
+++ commands/dhjk.py
@@ -1,35 +1,40 @@
+import os
 import discord
 from discord.ext import commands
 from discord import app_commands
-import random
 
-from service.interaction_handler import InteractionHandler
-from utils.misc_utils import stars
-from service.command import CommandService
+from utils.session_decorator import session_command
+from sessions.dhjk import DhjkSession
 
 
 class Dhjk(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
     self.logger = bot.logger
-    self.command = next((c for c in bot.static_data.commands if c['name'] == 'dhjk'), None)
-    self.messages = next((m for m in bot.static_data.messages if m['name'] == 'dhjk'), None)
-
-    CommandService.init_command(self.dhjk_app_command, self.command)
-
+    self.image_path = 'images/gifs'
+    self.images = self._get_images()
 
   @app_commands.command(name='dhjk')
+  @session_command(command_name='dhjk')
   async def dhjk_app_command(self, interaction: discord.Interaction):
-    self.logger.command_log('dhjk', interaction)
-    self.interaction_handler = InteractionHandler(self.bot)
-    await self.interaction_handler.send_wait_message(interaction=interaction)
-    response = Dhjk.get_response(self)
-    await self.interaction_handler.send_embed(interaction=interaction, response=response)
-    self.logger.ok_log('dhjk')
-
-  def get_response(self):
-    rand = random.randint(0, 4)
-    return {'title': stars(10), 'description': self.messages['items'][rand]['text'], 'color': 'blue', 'image': self.messages['items'][rand]['gif']}
+    session_data = {
+      'cog': self,
+      'interaction': interaction
+    }
+    session = DhjkSession(session_data)
+    await session.start()
+  
+  def _get_images(self) -> list:
+    images = []
+    if not os.path.isdir(self.image_path):
+      self.logger.log_only('warning', f'[DHJK] Unable to find folder : {self.image_path}')
+      return images
+    for filename in os.listdir(self.image_path):
+      if filename.lower().endswith('.gif'):
+        images.append(os.path.join(self.image_path, filename))
+    if not images:
+      self.logger.log_only('warning', f'[DHJK] No GIF found in {self.image_path}')
+    return images
   
 async def setup(bot):
   await bot.add_cog(Dhjk(bot))
