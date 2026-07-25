f62f9ef39ba9f5a118fd55bc993d21318cf34110
LucMagg <luc.maggiotto@gmail.com>
2026-07-25 17:28:40 +0200

    add set_trap
    -> new command to handle bot trap roles

---
+++ commands/set_trap.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+from utils.session_decorator import session_command
+from sessions.set_trap import Set_trapSession
+
+from utils.misc_utils import nick
+
+
+class Set_trap(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    self.logger = bot.logger
+
+  @app_commands.default_permissions(administrator=True)
+  @app_commands.command(name='set_trap')
+  @session_command(command_name='set_trap')
+  async def settraprole_app_command(self, interaction: discord.Interaction, trap_id: str):
+    self.logger.log_only('debug', f'[SET_TRAP] {nick(interaction)} setting trap role in channel {interaction.channel_id} : trap role id {trap_id}')
+    session_data = {
+      'cog': self,
+      'interaction': interaction,
+      'trap_id': trap_id
+    }
+    session = Set_trapSession(session_data)
+    await session.start()
+     
+async def setup(bot):
+  await bot.add_cog(Set_trap(bot))
