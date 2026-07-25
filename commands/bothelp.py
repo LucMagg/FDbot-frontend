52eabb1274747f4b6d1d25ad21168377a7ba0035
LucMagg <luc.maggiotto@gmail.com>
2026-07-25 17:19:31 +0200

    update bothelp
    -> refactor to handle the new bot's architecture with v2 features (translations/sessions/choices)

--- commands/bothelp.py
+++ commands/bothelp.py
@@ -2,28 +2,24 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-from service.command import CommandService
-from service.interaction_handler import InteractionHandler
-from utils.message import Message
+from utils.session_decorator import session_command
+from sessions.bothelp import BotHelpSession
 
 
 class Bothelp(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
     self.logger = bot.logger
-    self.interaction_handler = InteractionHandler(self.bot)
-    self.command = next((c for c in bot.static_data.commands if c['name'] == 'bothelp'), None)
-    self.help_msg = Message(bot).help('help')
-
-    CommandService.init_command(self.bothelp_app_command, self.command)
 
   @app_commands.command(name='bothelp')
+  @session_command(command_name='bothelp')
   async def bothelp_app_command(self, interaction: discord.Interaction):
-    self.logger.command_log('bothelp', interaction)
-    await self.interaction_handler.send_wait_message(interaction=interaction)
-    await self.interaction_handler.send_embed(interaction=interaction, response=self.help_msg)
-    self.logger.ok_log('bothelp')
-
+    session_data = {
+      'cog': self,
+      'interaction': interaction
+    }
+    session = BotHelpSession(session_data)
+    await session.start()
   
 async def setup(bot):
   await bot.add_cog(Bothelp(bot))
