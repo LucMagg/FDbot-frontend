ccda949e98f6fb7284c86fddc8d817e71474de40
LucMagg <luc.maggiotto@gmail.com>
2026-07-25 17:10:15 +0200

    update misc_utils
    -> delete pluriel & convert_seconds functions
    -> update rank_text to return rank in english

--- utils/misc_utils.py
+++ utils/misc_utils.py
@@ -21,42 +21,22 @@ def get_discord_color(color: str):
 
 def stars(how_many):
 	stars = ''
-	for iter in range(0, how_many):
+	for i in range(0, how_many):
 		stars += ':star:'
 	return stars
 
 def rank_text(number):
-	if number == 1:
-		return 'er'
-	else:
-		return 'ème'
-	
-def pluriel(elements: Union[List, int]) -> str:
-	if isinstance(elements, int):
-		if elements > 1:
-			return 's'
-		else:
-			return ''
-	if len(elements) > 1:
-		return 's'
-	return ''
+	match number:
+		case 1:
+			return 'st'
+		case 2:
+			return 'nd'
+		case 3:
+			return 'rd'
+	return 'th'
 
 def nick(message: discord.message) -> str:
 	nickname = message.user.nick
 	if nickname is None:
 		nickname = message.user.global_name
 	return nickname
-
-def convert_seconds(seconds) -> str:
-	to_return = []
-	hours = seconds // 3600
-	if hours > 0:
-		seconds = seconds % 3600
-		to_return.append(f'{hours} heures')
-	minutes = seconds // 60
-	if minutes > 0:
-		seconds = seconds % 60
-		to_return.append(f'{minutes} minutes')
-	if seconds > 0:
-		to_return.append(f'{seconds} secondes')
-	return ', '.join(to_return)
